5718817a53d02fa00d850586a809649e901c714c
tasniaarhamarham@gmail.com <tasniaarhamarham@gmail.com>
2026-05-12 18:28:20 +0600

    callbackFunction.go problem solved

--- module-5/callbackFunction.go
+++ module-5/callbackFunction.go
@@ -4,26 +4,32 @@ import "fmt";
 
 
 
-func callBack(a func(b func(c func())),x func(),y func()){
-	a(x);
+func callBack(a func(b func(c func()),d func()),x func(m func()),y func()){
+	
+	a(x,y);
+	
+	
 }
 
 func func1(){
  fmt.Println("last functions");
 }
 
-func func2(func1 func()){
+func func2(func4 func()){
 
-	func1();
+	 func4();
 
 }
 
-func func3(func2 func()){
-	func2()
+func func3(func22 func(funcNew func()),afunc func()){
+
+	 func22(afunc);
 }
 
 
 func main(){
+
+
 
 callBack(func3,func2,func1);
 	
