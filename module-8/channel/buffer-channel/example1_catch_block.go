24bba5064256e7543954a7c97d834da832c785fe
tasniaarhamarham@gmail.com <tasniaarhamarham@gmail.com>
2026-05-14 17:10:13 +0600

    buffer channel

--- module-8/channel/buffer-channel/example1_catch_block.go
+++ module-8/channel/buffer-channel/example1_catch_block.go
@@ -3,33 +3,49 @@ package main;
 import (
 	"fmt"
 	"time"
-	// "sync"
+	"sync"
 )
-// var wg sync.WaitGroup;
+var wg sync.WaitGroup;
 
 
 
 func main(){
-buffer_channel :=make(chan string,3);
-
+buffer_channel :=make(chan string,2);
+wg.Add(1)
 go func(){
+defer wg.Done();
 time.Sleep(2*time.Second);
 buffer_channel <- "I am funcion 1";
 }()
 
+wg.Add(1)
 go func(){
+defer wg.Done();
 time.Sleep(1*time.Second);
 buffer_channel <- "I am function 2";
 }()
 
+wg.Add(1)
 go func(){
+defer wg.Done();
 time.Sleep(3*time.Second);
 buffer_channel <- "I am function 3";
 }()
 
+wg.Add(1)
+go func(){
+defer wg.Done();
+time.Sleep(3*time.Second);
+buffer_channel <- "I am function 4";
+}()
 
-for range 3{
+
+
+for range 1{
 fmt.Println(<- buffer_channel);
 }
+wg.Wait();
+fmt.Println("Main End");
+
 
 }
